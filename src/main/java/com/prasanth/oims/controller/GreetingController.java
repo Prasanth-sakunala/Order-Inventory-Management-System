7d3926bd8ceacb5f3a15b9e975e15e67cbe9e2eb
prasanth sakunala <sakunalaprasanth@gmail.com>
2026-07-16 22:18:46 +0530

    updated instance load

--- src/main/java/com/prasanth/oims/controller/GreetingController.java
+++ src/main/java/com/prasanth/oims/controller/GreetingController.java
@@ -2,12 +2,13 @@ package com.prasanth.oims.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String greet() {
         return "Hello, welcome to the OIMS application!";
     }   
